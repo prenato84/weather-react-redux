988952884e2148d9a23eb9085e74dd5fc6de44b7
Paulo Castro <prenato84@gmail.com>
2020-04-29 10:18:30 -0300

    Adds test key for the API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "";
+const API_KEY = "e3cfed84b9eac5496042da684f6b77ef";
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
